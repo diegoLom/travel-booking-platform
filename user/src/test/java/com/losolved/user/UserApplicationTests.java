c0bb9a38195ef463b992796b120647bd8a537c61
diegoLom <diegosantana019@gmail.com>
2023-09-01 22:29:06 -0300

    User microservice creation

---
+++ user/src/test/java/com/losolved/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.losolved.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
