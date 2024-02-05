9a5d8d9b3e6a850a5564b801fb155cdeece49af6
diegoLom <diegosantana019@gmail.com>
2024-02-04 21:01:23 -0300

    Refactoring error handling

--- booking/src/main/java/com/losolved/booking/errorhandling/NoSuchBookingException.java
+++ booking/src/main/java/com/losolved/booking/errorhandling/NoSuchBookingException.java
@@ -3,10 +3,22 @@ package com.losolved.booking.errorhandling;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
-@AllArgsConstructor
+//@NoArgsConstructor
+//@AllArgsConstructor
 public class NoSuchBookingException extends RuntimeException {
 	
 	private String message;
+
+	public NoSuchBookingException() {
+		super();
+		this.message = "Booking not found";
+	}
+
+	public NoSuchBookingException(String message) {
+		super();
+		this.message = message;
+	}
+	
+	
 
 }
