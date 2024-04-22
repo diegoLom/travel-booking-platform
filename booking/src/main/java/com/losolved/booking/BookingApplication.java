1b8e0073ecfc5a790aa9f91fd5079419b7cf624d
dsantana <diego.santana@neogrid.com>
2024-04-22 17:14:53 -0300

    Config server implementation

--- booking/src/main/java/com/losolved/booking/BookingApplication.java
+++ booking/src/main/java/com/losolved/booking/BookingApplication.java
@@ -3,12 +3,16 @@ package com.losolved.booking;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import com.losolved.booking.configuration.DataSourceProperties;
+
 
 @SpringBootApplication
+@EnableConfigurationProperties(value = {DataSourceProperties.class})
 public class BookingApplication {
 
 	public static void main(String[] args) {
---
+++ booking/src/main/java/com/losolved/booking/configuration/DatabaseConfig.java
@@ -0,0 +1,30 @@
+package com.losolved.booking.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    private final DataSourceProperties dataSourceProperties;
+
+    @Autowired
+    public DatabaseConfig(DataSourceProperties dataSourceProperties) {
+        this.dataSourceProperties = dataSourceProperties;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl(dataSourceProperties.getUrl());
+        dataSource.setUsername(dataSourceProperties.getUsername());
+        dataSource.setPassword(dataSourceProperties.getPassword());
+        dataSource.setDriverClassName(dataSourceProperties.getDriverClassName());
+        return dataSource;
+    }
+}
