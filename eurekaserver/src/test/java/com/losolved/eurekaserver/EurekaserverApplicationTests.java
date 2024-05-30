ec3154eb8eed0d2e7181f75874bc3746538d6bb4
diegoLom <diegosantana019@gmail.com>
2024-05-30 15:51:18 -0300

    Eureka_Teste

--- eurekaserver/src/test/java/com/losolved/eurekaserver/EurekaserverApplicationTests.java
+++ eurekaserver/src/test/java/com/losolved/eurekaserver/EurekaserverApplicationTests.java
@@ -1,10 +1,10 @@
-package com.eazybytes.configserver;
+package com.losolved.eurekaserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ConfigserverApplicationTests {
+class EurekaserverApplicationTests {
 
 	@Test
 	void contextLoads() {
