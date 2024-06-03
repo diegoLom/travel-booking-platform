91da97b48bd5cbf4865434dcee6a5ce4b15bce3e
diegoLom <diegosantana019@gmail.com>
2024-06-03 16:10:58 -0300

    configurations files

--- booking/src/main/java/com/losolved/booking/configuration/DatabaseConfig.java
+++ booking/src/main/java/com/losolved/booking/configuration/DatabaseConfig.java
@@ -1,5 +1,7 @@
 package com.losolved.booking.configuration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +15,9 @@ import javax.sql.DataSource;
 public class DatabaseConfig {
 
     private final DataSourceProperties dataSourceProperties;
+    
+    private static final Logger logger = LoggerFactory.getLogger(DatabaseConfig.class);
+
 
     @Autowired
     public DatabaseConfig(DataSourceProperties dataSourceProperties) {
@@ -22,10 +27,21 @@ public class DatabaseConfig {
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        
+        logger.info(dataSourceProperties.getUrl());
+        logger.info(dataSourceProperties.getUsername());
+        logger.info(dataSourceProperties.getPassword());
+        logger.info(dataSourceProperties.getDriverClassName());
+        
+        
         dataSource.setUrl(dataSourceProperties.getUrl());
         dataSource.setUsername(dataSourceProperties.getUsername());
         dataSource.setPassword(dataSourceProperties.getPassword());
         dataSource.setDriverClassName(dataSourceProperties.getDriverClassName());
         return dataSource;
     }
+    
+    //TODO: See how to keep alive and run project (tail -f /dev/null && java -jar)
+    //TODO: see why s not running n debug 
+    //TODO: 
 }
