429421e76180de7b2ca539f41ec88f20132ffd78
dsantana <diego.santana@neogrid.com>
2024-07-20 20:23:42 -0300

    Adding changes

--- catalog/src/main/java/com/losolved/catalog/CatalogApplication.java
+++ catalog/src/main/java/com/losolved/catalog/CatalogApplication.java
@@ -2,8 +2,10 @@ package com.losolved.catalog;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
